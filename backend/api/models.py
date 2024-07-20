c212aa5b1ebda074c5e9043dc87d8bf5c2010945
lucinda003 <gayagoyw@gmail.com>
2024-07-21 01:00:08 +0800

    first commit

---
+++ backend/api/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField(default='')  # Provide a default value
+    email = models.EmailField()
+    contact_no = models.CharField(max_length=15)
+    birthday = models.DateField(null=True, blank=True)
+
+    employment_status = models.CharField(max_length=100, null=True, blank=True)
+    name_ext = models.CharField(max_length=10, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    place_of_birth = models.CharField(max_length=100, blank=True, null=True)
+    sex = models.CharField(max_length=10, blank=True, null=True)
+    civil_status = models.CharField(max_length=20, blank=True, null=True)
+    height_m = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    weight_kg = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    blood_type = models.CharField(max_length=10, blank=True, null=True)
+    gsis_no = models.CharField(max_length=20, blank=True, null=True)
+    pagibig_no = models.CharField(max_length=20, blank=True, null=True)
+    philhealth_no = models.CharField(max_length=20, blank=True, null=True)
+    sss_no = models.CharField(max_length=20, blank=True, null=True)
+    tin_no = models.CharField(max_length=20, blank=True, null=True)
+    agency_em = models.CharField(max_length=100, blank=True, null=True)
+    citizenship = models.CharField(max_length=50, blank=True, null=True)
+    residential_house_no = models.CharField(max_length=10, blank=True, null=True)
+    residential_street = models.CharField(max_length=100, blank=True, null=True)
+    residential_subd = models.CharField(max_length=100, blank=True, null=True)
+    residential_brgy = models.CharField(max_length=100, blank=True, null=True)
+    residential_city = models.CharField(max_length=100, blank=True, null=True)
+    residential_province = models.CharField(max_length=100, blank=True, null=True)
+    residential_zipcode = models.CharField(max_length=10, blank=True, null=True)
+    permanent_house_no = models.CharField(max_length=10, blank=True, null=True)
+    permanent_street = models.CharField(max_length=100, blank=True, null=True)
+    permanent_subd = models.CharField(max_length=100, blank=True, null=True)
+    permanent_brgy = models.CharField(max_length=100, blank=True, null=True)
+    permanent_city = models.CharField(max_length=100, blank=True, null=True)
+    permanent_province = models.CharField(max_length=100, blank=True, null=True)
+    permanent_zipcode = models.CharField(max_length=10, blank=True, null=True)
+    telephone = models.CharField(max_length=20, blank=True, null=True)
+    mobile_number = models.CharField(max_length=20, blank=True, null=True)
+    spouse_surname = models.CharField(max_length=100, blank=True, null=True)
+    spouse_first_name = models.CharField(max_length=100, blank=True, null=True)
+    spouse_middle_name = models.CharField(max_length=100, blank=True, null=True)
+    spouse_name_ext = models.CharField(max_length=10, blank=True, null=True)
+    spouse_occupation = models.CharField(max_length=100, blank=True, null=True)
+    spouse_employer = models.CharField(max_length=100, blank=True, null=True)
+    spouse_business_address = models.CharField(max_length=200, blank=True, null=True)
+    spouse_telephone = models.CharField(max_length=20, blank=True, null=True)
+    elementary_education = models.CharField(max_length=200, blank=True, null=True)
+    secondary_education = models.CharField(max_length=200, blank=True, null=True)
+    father_surname = models.CharField(max_length=100, blank=True, null=True)
+    father_first_name = models.CharField(max_length=100, blank=True, null=True)
+    father_middle_name = models.CharField(max_length=100, blank=True, null=True)
+    father_name_ext = models.CharField(max_length=10, blank=True, null=True)
+    mother_surname = models.CharField(max_length=100, blank=True, null=True)
+    mother_first_name = models.CharField(max_length=100, blank=True, null=True)
+    mother_middle_name = models.CharField(max_length=100, blank=True, null=True)
+    
+    
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    
